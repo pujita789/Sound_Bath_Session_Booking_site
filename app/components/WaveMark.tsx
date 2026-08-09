e48155218b670f241cb91b4a825c3cab11d8a930
Pujita Chakraborty <117858130+pujita789@users.noreply.github.com>
2026-08-09 22:40:29 +0530

    Delete app directory

--- app/components/WaveMark.tsx
+++
@@ -1,4 +0,0 @@
-/** Stillform brand mark, implemented with CSS so it inherits the surrounding color. */
-export function WaveMark() {
-  return <span className="wave-mark" aria-hidden="true"><i /><i /><i /></span>;
-}
